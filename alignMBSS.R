cf6c2373fc5c16387810c2f17097c0451c4c14d8
smcdonald125 <au51295@umbc.edu>
2020-07-23 17:44:52 +0000

    Added code to calculate % forest and % impervious within buffers

--- alignMBSS.R
+++ alignMBSS.R
@@ -1,4 +1,6 @@
-
+library(leaflet, lib.loc = "/usr/lib/R/site-library")
+library(raster, lib.loc = "/usr/lib/R/site-library")
+library(sf, lib.loc = "/usr/lib/R/site-library")
 
 setwd('/home/sarah/sesync_data/')
 
@@ -20,5 +22,54 @@ mbss_100 %>%
   addCircleMarkers(data = chwa_mbss, fillOpacity = 1, color = '#FF0000') %>% # add something from column in sf object as popup
   addPolygons(data = mbss_100, fillOpacity = 0, color = '#FFFFFF') %>%
   addMeasure(primaryLengthUnit = "meters") # fancy schmancy
+
+#read in raster
+LC <- raster("LC_MD_5m.tif")
+
+#aggregate raster data by buffer
+aggLC <- extract(LC, mbss_100) #store all pixel values for each buffer ... ?
+
+#Create empty vectors to store percent forest and percent impervious
+pctForest <- rep(0, length(aggLC))
+pctImp <- rep(0, length(aggLC))
+
+#Calculate percent forest cover and percent impervious and add to respective vectors
+for(row in 1:length(aggLC)){
+  tb <- as.data.frame(table(aggLC[row]))
+  forest <- tb[tb == 1, 'Freq']
+  imp <- tb[tb == 2, 'Freq'] #(tb[tb == 5, 'Freq'] + tb[tb == 6, 'Freq'])
+  if (length(forest) != 0){
+    pctForest[row] <- forest / length(aggLC[[row]]) #percent forested
+  }
+  if (length(imp) != 0){
+    pctImp[row] <- imp / length(aggLC[[row]])  # percent impervious
+  }
+}
+
+#add vectors as new columns to the spatial join of CHWA and MBSS
+#THIS IS THE DF TO USE IN OTHER SCRIPT - chwa_mbss
+chwa_mbss <- as.data.frame(chwa_mbss)
+chwa_mbss['BUFPctFor'] <- pctForest
+chwa_mbss['BUFPctImp'] <- pctImp
+
+
+#visualize buffers with calculated percentages over the land cover raster
+mbss_100['BUFPctFor'] <- pctForest
+pal <- colorNumeric(
+  palette = "Blues",
+  domain = mbss_100$BUFPctFor)
+mbss_100 %>%
+  leaflet() %>%
+  addProviderTiles(providers$Esri.WorldImagery) %>%
+  addPolygons(data = mbss_100, fillOpacity = 0.5, color=~pal(BUFPctFor), popup = ~BUFPctFor) #%>%
+  #addRasterImage(LC, colors = "Spectral")
+  #addMeasure(primaryLengthUnit = "meters") # fancy schmancy
+#addLayersControl(
+ # baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
+  #overlayGroups = c("Quakes", "Outline"),
+  #options = layersControlOptions(collapsed = FALSE)
+#)
+
+
 
 
