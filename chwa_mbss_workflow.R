fb0c2d4c5051b7a77ed0553fe767a598246e147e
lahm3d <lahmed@chesapeakebay.net>
2020-07-24 13:07:45 +0000

    Add data clean up

--- chwa_mbss_workflow.R
+++ chwa_mbss_workflow.R
@@ -1,8 +1,16 @@
-library(leaflet, lib.loc = "/usr/lib/R/site-library")
-library(raster, lib.loc = "/usr/lib/R/site-library")
-library(sf, lib.loc = "/usr/lib/R/site-library")
+library(dplyr)
+library(leaflet)
+library(raster)
+library(sf)
+library(foreign)
+library(data.table)
 
-setwd('/home/sarah/sesync_data/')
+# project directory
+
+proj_dir <- "/home/labeeb/sesync_healthy_watersheds"
+data_dir <- file.path(proj_dir, 'data')
+
+setwd(data_dir)
 
 #Read in MBSS point data and Healthy Watersheds Polygons
 mbss <- read_sf("MBSS.shp")
@@ -52,6 +60,81 @@ chwa_mbss <- as.data.frame(chwa_mbss)
 chwa_mbss['BUFPctFor'] <- pctForest
 chwa_mbss['BUFPctImp'] <- pctImp
 
+# Column lists to fix
+old_names <- c("SITEID", "YEAR1", "CBI_1", "FIBI_1", "BIBI_1",
+               "HYPERLIN_1", "HWUniqID", "HWFlag", "HWGroup", "CBPModAGN","CBPModAGP",
+               "CBPModAGS", "CBPModCSON", "CBPModCSOP", "CBPModCSOS",
+               "CBPModDEVN", "CBPModDEVP", "CBPModDEVS", "CBPModSEPN",
+               "CBPModSEPP", "CBPModSEPS", "CBPModWWN",  "CBPModWWP",
+               "CBPModWWS",  "ClimateStr", "HousingUni", "MineDensit", "OutletAqCo",
+               "PctAgOnHyd", "PctForestR", "PctForestW", "PctImpRZWs", "PctImpWs",
+               "WildfireRi", "MngdTurfHC", "PctForestL", "PctFores_1", "PctWetla_1",
+               "PctWetla_2", "DomesticWa", "Industrial", "RoadDensit", "Pct303dImp",
+               "HabConditi", "HabCondi_1", "HabCondi_2", "CatchmentA",
+               "WaterUseSu", "LandUseCha", "ClimateCha", "WaterQuali", "Geomorphol",
+               "HabitatSub", "LandCondSu", "BioCondSub", "HydrologyS", "WildfireSu",
+               "Vulnerabil", "Health_Ind", "Brook_Trou", "Brook_Tr_1"
+)
+new_names <- c ("MDSITEID", "YEAR", "MD_CUMBI", "MD_FIBI", "MD_BIBI", "HYPERLINK", "HWUID",
+                "CBP_SIHW", "HW_Outlet", "CBP_Ag_N", "CBP_Ag_P", "CBP_Ag_S", "CBP_CSO_N", "CBP_CSO_P","CBP_CSO_S",
+                "CBP_DEV_N", "CBP_DEV_P", "CBP_DEV_S", "CBP_SEP_N",
+                "CBP_SEP_P", "CBP_SEP_S", "CBP_WW_N",  "CBP_WW_P",
+                "CBP_WW_S",  "Cli_Stress", "HousUnit", "MineDensity", "Out_Aq_Con",
+                "PctAg_Hyd_S", "PctForeRZ", "PctForeWshed", "PctImpRZ", "PctImpWshed",
+                "Wld_Fir_Rsk", "Mngd_TF_HCZ", "PctForeLos", "PctFores_1", "PctWetWshed",
+                "PctWetla_2", "Dom_WatUse", "Ind_WatUse", "Road_Dens", "Pct_303d_Cat",
+                "HabConditi", "HabCondi_1", "HabCondi_2", "Catch_Area",
+                "WatUse_SIndx", "LUChange_SIndx", "ClimtChg_SIndx", "WatQual_SIndx", "Geomor_SIndx",
+                "Hab_SIndx", "LandCondSu", "BioCondSub", "HydrologyS", "WildfireSu",
+                "Vuln_SInx", "Health_Index", "BrkTrt_Curr", "BrkTrt_6deg"
+)
+new_columns <- c("OBJECTID", "MDSITEID", "YEAR", "MD_CUMBI", "MD_FIBI", "MD_BIBI", "HYPERLINK",
+                 "COMID", "HWUID", "CBP_SIHW", "HW_Outlet", "State", "County", "PctNatural", "PctForeRZ","PopDensity","HousUnit",
+                 "MineDensity","Mngd_TF_HCZ","PctForeLos", "LandCondSu","PctAg_Hyd_S","PctWetla_2","PctForeWshed", "PctImpWshed",
+                 "RoadStream", "PctForeLos","PctWetland","DamDensity","Road_Dens","PctWetWshed", "PctImpRZ","PctVulnGeo", "HabConditi","HabCondi_1", "HabCondi_2", "PctNatlCon",
+                 "Out_Aq_Con","Pct_303d_Cat","SPARROWTN", "SPARROWTP","CBP_Ag_N", "CBP_Ag_P","CBP_Ag_S", "CBP_CSO_N", "CBP_CSO_P", "CBP_CSO_S",
+                 "CBP_DEV_N", "CBP_DEV_P", "CBP_DEV_S", "CBP_SEP_N", "CBP_SEP_P", "CBP_SEP_S", "CBP_WW_N", "CBP_WW_P", "CBP_WW_S",
+                 "FutureDev","AvgPctFore", "PctProtLan", "AgWaterUse", "Dom_WatUse","Ind_WatUse","Wld_Fir_Rsk", "BrkTrt_Curr", "BrkTrt_6deg",
+                 "Cli_Stress", "HUC12_ID", "HUC12_Acre", "HUC12_DS", "HUC12_Name","HUC12_Type", "Headwater", "Catch_Area", "StateFIPS", "CountyFIPS",
+                 "FIPS",  "Shape_Leng", "Shape_Area", "BUFPctFor", "BUFPctImp")
+
+# fix columns
+chwa_mbss_df <- data.frame(chwa_mbss)
+chwa_mbss_df <- setnames(chwa_mbss_df, old=old_names, new=new_names, skip_absent = TRUE)
+chwa_mbss_df <- chwa_mbss_df[new_columns]
+write.csv(chwa_mbss_df, 'chwa_mbss.csv', row.names=FALSE)
+
+####
+
+df <- read.csv('chwa_mbss.csv')
+
+df %>%
+  # 1 - filter and excludes catchments with no data
+  filter(COMID > 0) %>%
+  # 2 - group by comid
+  group_by(COMID) %>%
+  # 3 - calculate min ranks 
+  summarize(mn_yr = min(YEAR)) -> ranks_df
+
+# 4 - merge ranks_df to df
+df <- merge(df, ranks_df, by = "COMID", all = TRUE)
+# 5 - calculate ranks using yr-(min_year-1)
+df['ranks'] <- df$YEAR - (df$mn_yr -1)
+# 6 - convert chars to numeric
+char_cols <- c("MD_CUMBI", "MD_FIBI", "MD_BIBI")
+df[char_cols] <- sapply(df[char_cols],as.numeric)
+
+df %>%
+  # 7 - use group-by on COMID
+  group_by(COMID) %>%
+  # 8 - summarize using weighted.mean()
+  mutate(wgt_mdcubi = weighted.mean(MD_CUMBI, ranks, na.rm = TRUE)) %>%
+  mutate(wgt_mdfibi = weighted.mean(MD_FIBI, ranks, na.rm = TRUE)) %>%
+  mutate(wgt_mdbibi = weighted.mean(MD_BIBI, ranks, na.rm = TRUE)) %>%
+  # 9 remove duplicates
+  distinct(COMID, .keep_all = TRUE) -> df
+
+############VISUALIZATION
 
 #visualize buffers with calculated percentages over the land cover raster
 mbss_100['BUFPctFor'] <- pctForest
@@ -62,14 +145,10 @@ mbss_100 %>%
   leaflet() %>%
   addProviderTiles(providers$Esri.WorldImagery) %>%
   addPolygons(data = mbss_100, fillOpacity = 0.5, color=~pal(BUFPctFor), popup = ~BUFPctFor) #%>%
-  #addRasterImage(LC, colors = "Spectral")
-  #addMeasure(primaryLengthUnit = "meters") # fancy schmancy
+#addRasterImage(LC, colors = "Spectral")
+#addMeasure(primaryLengthUnit = "meters") # fancy schmancy
 #addLayersControl(
- # baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
-  #overlayGroups = c("Quakes", "Outline"),
-  #options = layersControlOptions(collapsed = FALSE)
+# baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
+#overlayGroups = c("Quakes", "Outline"),
+#options = layersControlOptions(collapsed = FALSE)
 #)
-
-
-
-
